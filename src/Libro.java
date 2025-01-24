c8410e9c128a0ab54c8276febb9cbbd1c059292f
ronic <ralealp01@iesalbarregas.es>
2025-01-24 10:37:31 +0100

    creacion_constructores

---
+++ src/Libro.java
@@ -0,0 +1,31 @@
+public class Libro {
+    /*Agregar libros nuevos.(admin)
+Eliminar libros existentes.(admin)
+Buscar libros por título, autor o categoría.(todos)
+Mostrar todos los libros disponibles.(todos) */
+
+private String titulo;
+private String autor;
+private String categoria;
+private boolean prestado;
+
+
+Libro(){
+    titulo="";
+    autor="";
+    categoria="";
+    prestado=false;
+
+}
+
+Libro(String titulo, String autor, String categoria, boolean prestado){
+    this.titulo=titulo;
+    this.autor=autor;
+    this.categoria=categoria;
+    this.prestado=prestado;
+}
+
+
+
+    
+}
