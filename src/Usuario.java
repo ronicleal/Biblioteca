bd757f9bc76884de1cf49ec8a6a866dfea9ad1ce
diegogr94 <diegoguerreromil994@gmail.com>
2025-01-27 08:41:08 +0100

    ultimo cambio 1

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,19 +1,19 @@
-//TENGO QUE CREAR UN ARRAY DE USUARIOS Y LLAMARLO EN EL METODO DE CONSULTA
-
 public class Usuario {
     private String nombre;
     private String correo;
     private String password;
     private boolean rolAdministrador;
-    private String usuarioRegistrado;
-    
+
+    //no lo he hecho yo (sabia lo que tenia que hacer pero NO como formarlo)
+    private static Usuario[] usuariosRegistrados = new Usuario[100];
+    private static int contadorUsuarios = 0;
+    //hasta aqui
 
     Usuario() {
         this.nombre = " ";
         this.correo = " ";
         this.password = " ";
         this.rolAdministrador = true;
-        this.usuarioRegistrado = " ";
 
     }
 
@@ -22,7 +22,6 @@ public class Usuario {
         this.correo = correo;
         this.password = password;
         this.rolAdministrador = rolAdministrador;
-        this.usuarioRegistrado = usuarioRegistrado;
 
     }
 
@@ -46,11 +45,6 @@ public class Usuario {
         return this.rolAdministrador;
     }
 
-    public String getUsuarioRegistrado() {
-
-        return this.usuarioRegistrado;
-    }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
@@ -63,54 +57,47 @@ public class Usuario {
         this.password = password;
     }
 
-    public void setRol(boolean rolAdministrador) {
+    public void setRolAdministrador(boolean rolAdministrador) {
         this.rolAdministrador = rolAdministrador;
     }
 
-    public void setUsuarioRegristrado(String usuariosRegistrado) {
-        this.nombre = usuariosRegistrado;
-    }
-
-    // METODO PARA REGISTRAR USUARIOS SOLO SI ERES ADMINISTRADOR (no entiendo porque un usuario normal no se puede registrar por su cuenta)
+    // no entiendo porque tengo que registrar desde administrador... un usuario no deberia poder hacerlo por su cuenta???
     
-    public static boolean registrarUsuario (String nombre, String correo, String password, boolean rolAdministrador) {
-        
+    public static boolean registrarUsuario(String nombre, String correo, String password, boolean rolAdministrador) {
+
         if (!rolAdministrador) {
-            System.out.println("Los usuarios solo pueden ser registrados por un administrador");
+            System.out.println("Acceso denegado, tienes que ser administrador");
             return false;
+
+        } else {
+
+            //No lo he hecho yo
+
+            Usuario nuevoUsuario = new Usuario(nombre, correo, password, rolAdministrador);
+            usuariosRegistrados[contadorUsuarios] = nuevoUsuario;
+            contadorUsuarios++;
+
+            //Hasta aqui
+            System.out.println("Usuario registrado con exito");
+            return true;
+
         }
 
-        return true; 
-
-        
-}
-
-// METODO PARA CONSULTAR USUARIOS REGISTRADOS DESDE ADMINISTRADOR
-
-public void consultarUsuariosRegistrados() {
-    if (!this.rolAdministrador) {
-        System.out.println("Acceso denegado");
-        
     }
 
-    System.out.println("Lista de usuarios registrados: ");
-    //FOR PARA RECORRER Y MOSTRAR EL ARRAY CON LOS USUARIOS REGISTRADOS
     
+    public static void consultarUsuariosRegistrados(Usuario admin) {
+      
+        if (!admin.rolAdministrador) {
+            System.out.println("Acceso denegado, tienes que ser administrador");
+            
+        }
 
-
-
+        System.out.println("Lista de usuarios registrados: ");
+        for (int i = 0; i < contadorUsuarios; i++) {
+            Usuario usuario = usuariosRegistrados[i]; //en esta linea he necesitado ayuda
+            System.out.println("Nombre: " + usuario.nombre + ", Correo: " + usuario.correo + ", Rol: " //me daba error y buscando en internet me ha puesto "usuario.nombre etc"
+                    + (usuario.rolAdministrador ? "Administrador" : "Usuario"));
+        }
+    }
 }
-}
-
-
-
-
-
-
-    
-
-   
-
-
-
-
